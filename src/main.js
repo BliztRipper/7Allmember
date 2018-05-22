3c908c0cfaa35ecbf54323324b9aaf06ddc3e823
Arnon <nonthegenocide@me.com>
2018-05-22 10:42:29 +0700

    finish kyc precess

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 //@prepros-prepend ../node_modules/jquery/dist/jquery.min.js
 //@prepros-prepend ../node_modules/bootstrap/dist/js/bootstrap.bundle.js
-
 (function () {
   'use strict';
   window.addEventListener('load', function () {
