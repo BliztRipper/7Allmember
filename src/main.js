8620f80b6ed8f4b76982f4d55c87295dd7d8f169
Arnon <nonthegenocide@me.com>
2018-05-24 16:58:40 +0700

    add function check attr

--- src/main.js
+++ src/main.js
@@ -7,20 +7,29 @@
 (function () {
   'use strict';
   window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
+      form.addEventListener('keyup', function (event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
+        }else{
+          document.getElementById("check-invalid").removeAttribute("disabled");
         }
         form.classList.add('was-validated');
       }, false);
     });
   }, false);
 })();
+
+$('#styled-checkbox-2').click(function(){
+  var $list = $('.next--btn');
+  if ($list.attr('disabled')) {
+    $list.removeAttr('disabled');
+  } else {
+    $list.attr('disabled', 'disabled');
+  }
+})
 
 /* Signature Section */
 $('.js-signature').jqSignature();
